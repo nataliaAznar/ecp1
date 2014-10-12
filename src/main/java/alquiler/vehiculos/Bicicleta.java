ebfee2224a703f8373868db81f2c396304130a33
nataliaznar <nataliaznar@hotmail.com>
2014-10-12 21:11:35 +0200

    Implementación de la clase clases Bicicleta y Vehiculo implementadas

--- src/main/java/alquiler/vehiculos/Bicicleta.java
+++ src/main/java/alquiler/vehiculos/Bicicleta.java
@@ -1,17 +1,22 @@
 package alquiler.vehiculos;
 
 public class Bicicleta extends Vehiculo {
+	private static final double precio1 = 3;
+	private static final double precio2 = 2;
 	
 	public Bicicleta(String desc) {
 		super(desc);
 	}
 
 	public double getPrecio(int dias){
-		return 8;
+		int restoDias = dias - 2;
+		double precio = dias * Bicicleta.precio1;
+		if( restoDias > 0) precio += (restoDias * Bicicleta.precio2);
+		return precio;
 	}
 	
 	public String toString(){
-		return "Bici azul";		
+		return "Bici " + this.desc;		
 	}
 
 }
--- src/main/java/alquiler/vehiculos/Vehiculo.java
+++ src/main/java/alquiler/vehiculos/Vehiculo.java
@@ -1,9 +1,10 @@
 package alquiler.vehiculos;
 
 public class Vehiculo {
+	protected String desc;
 	
 	public Vehiculo( String desc){
-		
+		this.desc = desc;
 	}
 	
 	public double getPrecio(int dias){
